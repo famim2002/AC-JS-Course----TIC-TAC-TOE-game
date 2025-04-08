d17170f8b9f41305fb877951f775b0824255aa48
famim2002 <famimhayatyash@gmail.com>
2025-04-09 00:28:57 +0600

    add content

--- js/script.js
+++ js/script.js
@@ -13,13 +13,13 @@ let turnO = true;
 const winPatterns = [
     
     [0, 4, 8], //cross-line
-    [2, 4, 6] //cross-line
+    [2, 4, 6] ,//cross-line
     [0, 1, 2],
     [0, 3, 6],
     [1, 4, 7],
     [3, 4, 5],
     [2, 5, 8],
-    [8, 7, 6],
+    [8, 7, 6]
     
 ];
 
